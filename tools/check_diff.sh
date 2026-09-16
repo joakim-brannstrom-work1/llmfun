d1e0ec20ece0fa02ea75fc8f3a6bbebfcf255013
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2026-09-16 21:30:38 +0200

    tool: fix diff check

--- tools/check_diff.sh
+++ tools/check_diff.sh
@@ -5,6 +5,8 @@ diff -r local_model/source llmfun/workarea/llmfun/local_model/source
 diff -r common llmfun/workarea/llmfun/common/source
 diff -r vendor llmfun/workarea/llmfun/vendor
 diff -r cpp_tui llmfun/workarea/llmfun/cpp_tui
+diff -r llmfun/config llmfun/workarea/llmfun/llmfun/config
+diff -r llmfun/data llmfun/workarea/llmfun/llmfun/data
 diff  doc/ llmfun/workarea/llmfun/doc
 diff  config/ llmfun/workarea/llmfun/config
 diff  AGENTS.md llmfun/workarea/llmfun/AGENTS.md
